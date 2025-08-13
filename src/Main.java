7f79f8f06b701ad5e5c095b440379dbe57cd3f8a
yuw4ns <161660991+yuw4ns@users.noreply.github.com>
2025-08-13 16:40:15 +1000

    Finished task 4 and 5

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,90 @@
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+class Cell {
+    private int x, y, size;
+
+    public Cell(int x, int y, int size) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+    }
+
+    public void paint(Graphics g, boolean highlighted) {
+        if (highlighted) {
+            g.setColor(Color.GRAY);
+            g.fillRect(x, y, size, size);
+        }
+        g.setColor(Color.BLACK);
+        g.drawRect(x, y, size, size);
+    }
+
+    public boolean contains(int mx, int my) {
+        return mx >= x && mx < x + size && my >= y && my < y + size;
+    }
+}
+
+class Grid {
+    private int rows, cols;
+    private Cell[][] cells;
+
+    public Grid(int rows, int cols, int cellSize, int startX, int startY) {
+        cells = new Cell[rows][cols];
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                cells[r][c] = new Cell(
+                    startX + c * cellSize,
+                    startY + r * cellSize,
+                    cellSize
+                );
+            }
+        }
+    }
+
+    public void paint(Graphics g, Point mouse) {
+        int mx = mouse != null ? mouse.x : -1;
+        int my = mouse != null ? mouse.y : -1;
+        for (int r = 0; r < cells.length; r++) {
+            for (int c = 0; c < cells[r].length; c++) {
+                boolean highlighted = cells[r][c].contains(mx, my);
+                cells[r][c].paint(g, highlighted);
+            }
+        }
+    }
+}
+
 public class Main extends JFrame {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Main window = new Main();
         window.run();
     }
 
     class Canvas extends JPanel {
+        private Grid grid;
+        private Point mouse = null;
+
         public Canvas() {
             setPreferredSize(new Dimension(720, 720));
+            grid = new Grid(20, 20, 35, 10, 10);
+
+            addMouseMotionListener(new MouseMotionAdapter() {
+                public void mouseMoved(MouseEvent e) {
+                    mouse = e.getPoint();
+                    repaint();
+                }
+            });
         }
 
         @Override
         public void paint(Graphics g) {
             super.paint(g);
-
-            int rows = 20;
-            int cols = 20;
-            int cellSize = 35;
-            int startX = 10;
-            int startY = 10;
-
-            // Draw the grid lines
-            for (int r = 0; r <= rows; r++) {
-                int y = startY + r * cellSize;
-                g.drawLine(startX, y, startX + cols * cellSize, y);
-            }
-            for (int c = 0; c <= cols; c++) {
-                int x = startX + c * cellSize;
-                g.drawLine(x, startY, x, startY + rows * cellSize);
-            }
+            grid.paint(g, mouse);
         }
     }
 
@@ -45,8 +97,5 @@ public class Main extends JFrame {
     }
 
     public void run() {
-        while (true) {
-            repaint();
-        }
     }
 }
